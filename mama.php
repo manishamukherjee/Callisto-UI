a0dafb65781610af7f5ec6992b85225dd2962583
Manisha Mukherjee (manimukh) <manimukh@cisco.com>
2016-02-16 16:55:12 -0800

    added img and json files

---
+++ mama.php
@@ -0,0 +1,2 @@
+{ "records":[{"intfName" : "TenGigE0/0/0/0","IPAddress" : "1.2.3.4","Status" : "Up","Description" : "This is a router"	}, 
+{"intfName" : "TenGigE0/0/0/1","IPAddress" : "1.2.3.5","Status" : "Down","Description" : "This is a router"}] }
---
+++ overlay.php
@@ -0,0 +1,2 @@
+{ "records":[{"intfName" : "TenGigE0/0/0/100","IPAddress" : "1.2.3.4","Status" : "Up","Description" : "This is a router"	}, 
+{"intfName" : "TenGigE0/0/0/200","IPAddress" : "1.2.3.5","Status" : "Down","Description" : "This is a router"}] }
