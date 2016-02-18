e066a2f79a1f95c56ee8f690b3971745da547077
Manisha Mukherjee (manimukh) <manimukh@cisco.com>
2016-02-17 17:10:07 -0800

    topology functional

--- app/topology/topology.js
+++ app/topology/topology.js
@@ -1,76 +1,99 @@
-topo = new nx.graphic.Topology({
-  adaptive: true,
-  nodeConfig: {
-    label: 'model.id'
-  },
-  showIcon: true,
-  theme: 'yellow'
+//initialize a topology component
+var topo = new nx.graphic.Topology({
+    width: 800,
+    height: 800,
+    showIcon: true,
+    nodeConfig: {
+        label: 'model.name',
+        iconType:function(vertex) {
+            var id = vertex.get("id");
+            if (id <4) {
+                return 'router'
+            } 
+            else{
+            	return 'cloud'
+            }
+        }
+    },
+    linkConfig: {
+        linkType: 'parallel'
+    }
 });
 
-
+//set data
 topo.on('ready', function() {
-  topo.data(topologyData);
-})
+    topo.data(topologyData);
+});
 
+//create app
 var app = new nx.ui.Application();
+//attach topo to app;
 topo.attach(app);
 
-
-addNewLink = function() {
-
-  topo.addLink({
-    source: 0,
-    target: 4
-  })
-
-  // update linkSet
-
-  var edgeSet = topo.graph().getEdgeSetBySourceAndTarget(0, 4)
-  if (edgeSet) {
-    topo.graph()._generateConnection(edgeSet)
-  }
-
-}
-
-insetNodeSet = function() {
-  topo.aggregationNodes([4, 3])
-}
-
-
 var topologyData = {
-  nodes: [{
-    "id": 0,
-    "x": 410,
-    "y": 100,
-    "name": "12K-1"
-  }, {
-    "id": 1,
-    "x": 410,
-    "y": 280,
-    "name": "12K-2"
-  }, {
-    "id": 2,
-    "x": 660,
-    "y": 280,
-    "name": "Of-9k-03"
-  }, {
-    "id": 3,
-    "x": 660,
-    "y": 100,
-    "name": "Of-9k-02"
-  }, {
-    "id": 4,
-    "x": 180,
-    "y": 190,
-    "name": "Of-9k-01"
-  }],
-  links: [],
-  nodeSet: [{
-    id: 5,
-    nodes: [0, 1],
-    "x": 300,
-    "y": 300,
-    "name": "Node set 1",
-    iconType: 'router'
-  }]
+    nodes: [{
+        "id": 0,
+        "x": 410,
+        "y": 100,
+        "IP":"1.2.3.0",
+        "name": "A"
+    }, {
+        "id": 1,
+        "x": 410,
+        "y": 280,
+        "IP":"1.2.3.1",
+        "name": "B"
+    }, {
+        "id": 2,
+        "x": 660,
+        "y": 280,
+        "IP":"1.2.3.2",
+        "name": "C"
+    }, {
+        "id": 3,
+        "x": 660,
+        "y": 100,
+        "IP":"1.2.3.3",
+        "name": "D"
+    }, {
+        "id": 4,
+        "x": 180,
+        "y": 190,
+        "IP":"1.2.3.4",
+        "name": "E"
+    }],
+    links: [{
+        "source": 0,
+        "target": 1
+    }, {
+        "source": 1,
+        "target": 2
+    }, {
+        "source": 1,
+        "target": 3
+    }, {
+        "source": 4,
+        "target": 1
+    }, {
+        "source": 2,
+        "target": 3
+    }, {
+        "source": 2,
+        "target": 0
+    }, {
+        "source": 3,
+        "target": 0
+    }, {
+        "source": 3,
+        "target": 0
+    }, {
+        "source": 3,
+        "target": 0
+    }, {
+        "source": 0,
+        "target": 4
+    }, {
+        "source": 0,
+        "target": 4
+    }]
 };
